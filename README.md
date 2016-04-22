95ec18238729a6ea6b30a701847f68649c97462f
Igor Alpert <IAlpert>
2016-04-21 23:14:59 -0400

    test(1st): Test for the plugin

---
+++ README.md
@@ -0,0 +1,4 @@
+vorpal-as-default
+====
+
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
---
+++ test/index.js
@@ -0,0 +1,112 @@
+import expect from 'expect';
+import { stdout, stderr } from 'test-console';
+import asDefault from '../src/index.js';
+import Vorpal from 'vorpal';
+
+function getCLI() {
+  let vorpal = Vorpal();
+
+  vorpal
+    .command('task1')
+    .action(function (args, next) {
+      this.log('task1');
+      next();
+    });
+
+  vorpal
+    .command('task2')
+    .action(function (args, next) {
+      this.log('task2');
+      next();
+    });
+
+
+  vorpal
+    .command('task3')
+    .action(function (args, next) {
+      this.log('task3');
+      next();
+    });
+
+  vorpal
+    .delimiter('')
+    .show();
+
+  return vorpal;
+}
+
+
+describe('Vorpal', function () {
+
+  it('Should be an instance', ()=> {
+    let instance = getCLI();
+    expect(instance).toBeA(Vorpal);
+  });
+
+});
+
+
+describe('As Default - plugin for Vorpal', function () {
+
+  it('Should be a function', ()=> {
+    expect(asDefault).toBeA(Function);
+  });
+
+
+  it('Should be a Vorpal plugin - not throwing exceptions', () => {
+    expect(() => {
+      getCLI().use(asDefault, 'task1');
+    }).toNotThrow();
+  });
+
+});
+
+
+describe('Act as default command', function () {
+
+  it('Should execute CLI without default command', () => {
+
+    const out = stdout.inspectSync(() => {
+      expect(() => {
+        getCLI().use();
+      }).toNotThrow();
+
+    });
+
+    expect(out).toExclude('task1\n');
+    expect(out).toExclude('task2\n');
+    expect(out).toExclude('task3\n');
+
+  });
+
+
+  it('Should execute CLI with default command', () => {
+
+    const out = stdout.inspectSync(() => {
+      expect(() => {
+        getCLI().use(asDefault, 'task2');
+      }).toNotThrow();
+
+    });
+
+    expect(out).toExclude('task1\n');
+    expect(out).toExclude('task3\n');
+
+  });
+
+
+  it('Should execute CLI with defined command', () => {
+
+    const out = stdout.inspectSync(() => {
+      expect(() => {
+        getCLI().exec('task3');
+      }).toNotThrow();
+
+    });
+
+    expect(out).toExclude('task1\n');
+    expect(out).toExclude('task2\n');
+
+  });
+
+});
